8ea236d780801a60e9eb509eeb10c739a3d71350
TrsRyan <324586415+TrsRyan@users.noreply.github.com>
2026-09-12 12:09:12 +0200

    chore: pin sanity studio appId (#3)

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -22,5 +22,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    appId: 'ntvz0k435dn2n1ve71crsz1x',
   },
 })
